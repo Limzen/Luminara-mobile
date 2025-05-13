1e388b75998ea90c1f956de760a9970532a41bca
IOvOI <m.rivaldiys@gmail.com>
2025-05-13 20:55:59 +0700

    Add login and sign up screens

--- app/src/main/java/com/example/luminara/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/luminara/ui/screens/login/LoginScreen.kt
@@ -1,21 +1,121 @@
 package com.example.luminara.ui.screens.login
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.luminara.ui.components.AuthButton
+import com.example.luminara.ui.components.AuthTextField
+import com.example.luminara.ui.theme.BlueText
 
 @Composable
 fun LoginScreen() {
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 40.dp)
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.Start
     ) {
-        Text("Login", style = MaterialTheme.typography.displayLarge)
+        Text(
+            text = "Welcome 👋",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineLarge,
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Today is a new day. It's your day. You shape it. Sign in to start managing your projects.",
+            style = MaterialTheme.typography.titleMedium,
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "Email",
+            style = MaterialTheme.typography.titleMedium
+        )
+        AuthTextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = "Example@gmail.com",
+
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Password",
+            style = MaterialTheme.typography.titleMedium
+        )
+        AuthTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = "At least 8 characters",
+            isPassword = true,
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Forgot password?",
+            style = MaterialTheme.typography.titleMedium,
+            color = BlueText,
+            modifier = Modifier
+                .align(Alignment.End)
+                .clickable {}
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        AuthButton(
+            onClick = { /* TODO */ },
+            text = "Login",
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Row (
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(
+                text = "Don't you have an account?",
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.width(6.dp))
+
+            Text(
+                text = "Sign Up",
+                style = MaterialTheme.typography.titleSmall,
+                color = BlueText,
+                modifier = Modifier
+                    .clickable{}
+            )
+        }
+
+
     }
 }
+
--- app/src/main/java/com/example/luminara/ui/screens/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/luminara/ui/screens/signup/SignUpScreen.kt
@@ -1,2 +1,135 @@
 package com.example.luminara.ui.screens.signup
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.luminara.ui.components.AuthButton
+import com.example.luminara.ui.components.AuthTextField
+import com.example.luminara.ui.theme.BlueText
+
+
+@Composable
+fun SignUpScreen (){
+    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 40.dp)
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "Sign Up",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineLarge,
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "Username",
+            style = MaterialTheme.typography.titleMedium,
+        )
+
+        AuthTextField(
+            value = username,
+            onValueChange = { username = it },
+            placeholder = "Username",
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Email",
+            style = MaterialTheme.typography.titleMedium,
+        )
+
+        AuthTextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = "Example@gmail.com",
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Password",
+            style = MaterialTheme.typography.titleMedium,
+        )
+
+        AuthTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = "At least 8 characters",
+            isPassword = true
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Confirm Password",
+            style = MaterialTheme.typography.titleMedium,
+        )
+
+        AuthTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            placeholder = "Confirm Password",
+            isPassword = true
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        AuthButton(
+            onClick = { /* TODO */ },
+            text = "Sign Up",
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Row(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(
+                text = "Already have an account?",
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.width(6.dp))
+
+            Text(
+                text = "Sign In",
+                style = MaterialTheme.typography.titleSmall,
+                color = BlueText,
+                modifier = Modifier
+                    .clickable {}
+            )
+
+
+        }
+    }
+}
+
