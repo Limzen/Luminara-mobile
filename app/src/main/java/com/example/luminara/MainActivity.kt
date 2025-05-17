d8f60927e3dc9ac7852df46ed065243434a5a346
Daffaazzachrie7 <Daffaazzachrie@gmail.com>
2025-05-17 22:38:46 +0700

    add profile Screen

--- app/src/main/java/com/example/luminara/MainActivity.kt
+++ app/src/main/java/com/example/luminara/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.luminara.ui.screens.login.LoginScreen
-import com.example.luminara.ui.screens.signup.SignUpScreen
+import com.example.luminara.ui.screens.profile.ProfileScreen
 import com.example.luminara.ui.theme.LuminaraTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LuminaraTheme {
-                LoginScreen()
+                ProfileScreen()
             }
         }
     }
