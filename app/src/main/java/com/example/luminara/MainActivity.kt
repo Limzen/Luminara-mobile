08aac046f6d1d3409f258bec038605bf5f4ad2af
unknown <andrycoding@gmail.com>
2025-05-19 23:39:55 +0700

    fix itinerary

--- app/src/main/java/com/example/luminara/MainActivity.kt
+++ app/src/main/java/com/example/luminara/MainActivity.kt
@@ -5,11 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.luminara.ui.screens.itinerary.CreateItinerary
-<<<<<<< HEAD
 import com.example.luminara.ui.screens.home.HomeScreen
-=======
 import com.example.luminara.ui.screens.itinerary.FormItinerary
->>>>>>> 097eeec990a29f5f2701430f5299514c7a1441b6
 import com.example.luminara.ui.screens.login.LoginScreen
 import com.example.luminara.ui.screens.signup.SignUpScreen
 import com.example.luminara.ui.screens.profile.ProfileScreen
